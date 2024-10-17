9e775c4d71923449f8722e6ef2dbef5b70292c40
Kevin Nguyen <nguyenkevin625@gmail.com>
2024-10-17 05:15:06 +0000

    Add basic CD74HC4067SM96 and Analog Sensor implementation, Add CMake build process

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,60 @@
-
+// Pico Includes
 #include "pico/stdlib.h"
+#include "hardware/adc.h"
+#include <iostream>
+
+#include "GpioManager.h"
+
+// Config
+#include "CD74HC4067SM96_00_config.h"
+#include "CD74HC4067SM96_01_config.h"
+#include "CD74HC4067SM96_02_config.h"
+#include "CD74HC4067SM96_03_config.h"
+#include "CD74HC4067SM96_gpio_selector.h"
+
+// Drivers
+#include "CD74HC4067SM96.h"
+
+#define ADC_TRIGGER_THRESHOLD 3000
+#define GPIO_TEST_00 15
+#define GPIO_TEST_01 16
 
 int main() {
+    // Debug
+    // stdio_init_all();
 
+    // Initialize GPIO
+    adc_init();
+    CD74HC4067SM96 MUX_00 = CD74HC4067SM96(CD74HC4067SM96_00_SIGNAL, GPIO_SELELECTOR_00, GPIO_SELELECTOR_01, GPIO_SELELECTOR_02, GPIO_SELELECTOR_03, CD74HC4067SM96_00_MAPPING);
+    CD74HC4067SM96 MUX_01 = CD74HC4067SM96(CD74HC4067SM96_01_SIGNAL, GPIO_SELELECTOR_01, GPIO_SELELECTOR_01, GPIO_SELELECTOR_02, GPIO_SELELECTOR_03, CD74HC4067SM96_01_MAPPING);
+    CD74HC4067SM96 MUX_02 = CD74HC4067SM96(CD74HC4067SM96_02_SIGNAL, GPIO_SELELECTOR_01, GPIO_SELELECTOR_01, GPIO_SELELECTOR_02, GPIO_SELELECTOR_03, CD74HC4067SM96_02_MAPPING);
+    CD74HC4067SM96 MUX_03 = CD74HC4067SM96(CD74HC4067SM96_03_SIGNAL, GPIO_SELELECTOR_01, GPIO_SELELECTOR_01, GPIO_SELELECTOR_02, GPIO_SELELECTOR_03, CD74HC4067SM96_03_MAPPING);
+
+
+    gpio_init(GPIO_TEST_00);
+    gpio_set_dir(GPIO_TEST_00, GPIO_OUT);
+    gpio_init(GPIO_TEST_01);
+    gpio_set_dir(GPIO_TEST_01, GPIO_OUT);
+
+    while(true) {
+        for(size_t i = 0; i < CD74HC4067SM96_NUM_CHANNELS; i++) {
+                if(CD74HC4067SM96_00_ENABLED && MUX_00.readChannel(i) > ADC_TRIGGER_THRESHOLD) {
+                    gpio_put(GPIO_TEST_00, true);
+                } else {
+                    gpio_put(GPIO_TEST_00, false);
+                }
+
+                if(CD74HC4067SM96_01_ENABLED && MUX_01.readChannel(i) > ADC_TRIGGER_THRESHOLD) {
+                    gpio_put(GPIO_TEST_01, true);
+                } else {
+                    gpio_put(GPIO_TEST_01, false);
+                }
+
+            // if(MUX_01.readChannel(i) > ADC_TRIGGER_THRESHOLD) {
+            //     gpio_put(GPIO_TEST_01, true);
+            // } else {
+            //     gpio_put(GPIO_TEST_01, false);
+            // }
+        }
+    }
 }
